a254fb93515564f7d6537d1a15827e1382239540
Alan <alanalcaniz24@gmail.com>
2026-08-08 17:13:29 +0200

    favicon añadido

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,13 @@
+import { Metadata } from "next";
 import "./globals.css";
+
+export const metadata: Metadata = {
+  title: 'Alan Alcañiz | Portfolio',
+  description: 'Portfolio web de desarrollo frontend',
+  icons: {
+    icon: '/favicon.ico', // Tu favicon
+  },
+};
 
 export default function RootLayout({
   children,
